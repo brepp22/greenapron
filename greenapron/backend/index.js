572c6d698c0d59911de134458dab6baef7484206
brepp22 <brepp@hotmail.com>
2025-05-01 17:37:33 -0700

    server port updated

--- greenapron/backend/index.js
+++ greenapron/backend/index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
   res.send('API is up and running!');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
