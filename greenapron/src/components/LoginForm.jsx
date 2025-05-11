2aa114dc2f16942f42eb49c9f73d47a2b8fcc4de
brepp22 <brepp@hotmail.com>
2025-05-11 14:30:17 -0700

    loginform css created

--- greenapron/src/components/LoginForm.jsx
+++ greenapron/src/components/LoginForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import './LoginForm.css';
 
 const LoginForm = () => {
   const [form, setForm] = useState({ email: '', password: '' });
