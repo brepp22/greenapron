44cfe08eeb65ed90c56b33cd6c94b3ce62d753be
brepp22 <brepp@hotmail.com>
2025-05-24 22:53:04 -0700

    register option added to frontend

--- greenapron/src/components/LoginForm.jsx
+++ greenapron/src/components/LoginForm.jsx
@@ -6,6 +6,7 @@ import './LoginForm.css';
 const LoginForm = ({setToken}) => {
   const [form, setForm] = useState({ email: '', password: '' });
   const [error, setError] = useState('');
+  const [isRegister, setIsRegister] = useState('');
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -16,21 +17,25 @@ const LoginForm = ({setToken}) => {
     e.preventDefault();
     setError('');
 
+    const endpoint = isRegister ?
+      'http://localhost:8080/api/users/register' :
+      'http://localhost:8080/api/users/login';
+
 
     try {
-      const res = await axios.post('http://localhost:8080/api/users/login', form);
-      console.log('Login success, token:', res.data.token); 
+      const res = await axios.post(endpoint, form);
+      console.log(`${isRegister ? 'Registration' : 'Login'}`, res.data.token); 
       localStorage.setItem('token', res.data.token);
       setToken(res.data.token); 
       navigate('/board'); 
     } catch (err) {
-      setError(err.response?.data?.message || 'Login failed');
+      setError(err.response?.data?.message || `${isRegister ? 'Registration' : 'Login'}`);
     }
   };
 
   return (
     <form onSubmit={handleSubmit} className="login-form">
-      <h2>Login</h2>
+      <h2>{isRegister ? 'Register' : 'Login'}</h2>
 
       <input
         className="input-email"
@@ -52,8 +57,18 @@ const LoginForm = ({setToken}) => {
         required
       /><br />
 
-      <button className='login-button' type="submit">Login</button>
+      <button className='login-button' type="submit">{isRegister ? 'Register' : 'Login'}</button>
       {error && <p className="error-message" style={{ color: 'red' }}>{error}</p>}
+
+       <p style={{ marginTop: '10px' }}>
+        {isRegister ? 'Already have an account?' : "Don't have an account?"}{' '}
+        <span
+          style={{ color: '#006142', cursor: 'pointer', textDecoration: 'underline' }}
+          onClick={() => setIsRegister(prev => !prev)}
+        >
+          {isRegister ? 'Login' : 'Register'}
+        </span>
+      </p>
     </form>
   );
 };
