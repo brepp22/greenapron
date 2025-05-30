77624fb9774dfeab5e9cb217aa3aae045aea4580
brepp22 <brepp@hotmail.com>
2025-05-30 13:12:54 -0700

    message posted linked with profile

--- greenapron/backend/index.js
+++ greenapron/backend/index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const messagesRouter = require('./routes/messageRouter');
 const usersRouter = require('./routes/userRouter'); 
 const app = express();
+const authenticate = require('./middleware/authenticate');
+const db = require('./db')
 
 
 
@@ -21,15 +23,24 @@ app.use(express.json());
 app.use('/api/users', usersRouter);
 app.use('/api/messages', messagesRouter)
 
-// app.get('/', (req, res) => {
-//   res.send('API is running');
-// });
-
 
 
 app.get('/', (req, res) => {
   res.send('API is up and running!');
 });
+
+app.get('/api/profile', authenticate, async (req, res) => {
+  try {
+    const user = await db('users').where({ id: req.user.id }).first();
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    res.json({ id: user.id, name: user.name }); 
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
