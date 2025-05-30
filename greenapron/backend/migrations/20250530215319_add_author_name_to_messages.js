1288085a5c68d61efcaeeb44f1a94eb7f944b5ca
brepp22 <brepp@hotmail.com>
2025-05-30 14:54:58 -0700

    migration added to store profile name to messages in backend

---
+++ greenapron/backend/migrations/20250530215319_add_author_name_to_messages.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('messages', (table) => {
+    table.string('name'); 
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('messages', (table) => {
+    table.dropColumn('name');
+  });
+};
